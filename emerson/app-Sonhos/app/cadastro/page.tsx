544e34c27ed1e3523b9f9240f723eb30cac1f5fb
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-08-31 21:45:01 +0100

    Add Link import to Cadastro component

--- emerson/app-Sonhos/app/cadastro/page.tsx
+++ emerson/app-Sonhos/app/cadastro/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+
 import Link from "next/link";
 import "../auth.css"; // Mantenha seu CSS externo se tiver, ou use inline como abaixo
 export default function Cadastro() {
